f332c1c80f5d8c35280bbe233f914e4badd95ca0
Bry Cox <carl.cc@gmail.com>
2019-08-07 22:43:19 +0000

    Automatically backed up by Learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,6 +1,11 @@
 class CouponsController < ApplicationController
 
   def create
+    @coupon = Coupon.new
+    @coupon.coupon_code = params[:coupon_code]
+    @coupon.store = params[:store]
+    @coupon.save
+    redirect_to student_path(@student)
   end
 
 end
